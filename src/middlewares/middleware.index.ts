3e1f82c697134c229f201e17f7d561bdf0cb2a43
Shashi Kant <shashi3.kant@paytm.com>
2025-01-31 17:18:08 +0530

    added login sign up and user type api

--- src/middlewares/middleware.index.ts
+++ src/middlewares/middleware.index.ts
@@ -1,6 +1,7 @@
 
+import { errors } from "celebrate"; // Import celebrate's error handler
 import cors from 'cors'
-import  express, { Express } from 'express'
+import  express, { Express , Request, Response, NextFunction } from 'express'
 import bodyParser from 'body-parser'
 export const middlewares = {
 
@@ -12,7 +13,21 @@ export const middlewares = {
     globalMiddlewares: function (app:Express){
         app.use(cors(this.corsOptions))
         app.use(bodyParser.urlencoded({ extended: false }))
-        app.use(express.json())        
+        app.use(express.json()) 
+        app.use(errors()); 
+        app.use((err: any, req: Request, res: Response, next: NextFunction):any => {
+            if (err.joi) {
+              return res.status(400).json({
+                status: "error",
+                message: "Validation failed",
+                details: err.joi.details.map((detail: any) => detail.message), // Extracts Joi validation errors
+              });
+            }
+          
+            // If it's not a Celebrate error, pass it to the default handler
+            next(err);
+          });
+
     }
     
 }
