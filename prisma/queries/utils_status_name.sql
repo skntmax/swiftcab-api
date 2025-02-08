21916e38c35ebd596c0d5fcada82682ad79c7ec2
shashi kant <skntjee@gmail.com>
2025-02-08 14:59:39 +0530

    added

---
+++ prisma/queries/utils_status_name.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO public.utils_status_names ("name", created_on, updated_on)
+VALUES
+    ('Guid', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Food', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Water', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
