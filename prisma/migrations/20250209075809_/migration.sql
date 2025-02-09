ab82aaf8fe129015d0c53f37c53a7b6197aae534
shashi kant <skntjee@gmail.com>
2025-02-09 14:28:37 +0530

    added

--- prisma/migrations/20250209075809_/migration.sql
+++ prisma/migrations/20250209075809_/migration.sql
@@ -10,7 +10,6 @@
 
 */
 -- AlterTable
-ALTER TABLE "users_have_rides" ADD COLUMN     "is_food" INTEGER NOT NULL,
 ADD COLUMN     "is_guide" INTEGER NOT NULL,
 ADD COLUMN     "is_water" INTEGER NOT NULL;
 
