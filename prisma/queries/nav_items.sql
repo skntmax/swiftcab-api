8ee2ffe83556de51a167c2b80be78cbef92b4024
shashi kant <skntjee@gmail.com>
2025-02-20 02:32:07 +0530

    added navbar

--- prisma/queries/nav_items.sql
+++ prisma/queries/nav_items.sql
@@ -2,4 +2,15 @@ INSERT INTO nav_items (nav_item, sub_menu, href, icon, created_on, updated_on) V
 ('Vhicles', true, NULL, NULL, NOW(), NOW()),
 ('Rides', true, NULL, NULL, NOW(), NOW()),
 ('Master', true, NULL, NULL, NOW(), NOW()),
-('Settlements', true, NULL, NULL, NOW(), NOW());
+('Settlements', true, NULL, NULL, NOW(), NOW()),
+
+-- for admin 
+('Customers', true, NULL, NULL, NOW(), NOW()),
+('Owners', true, NULL, NULL, NOW(), NOW()),
+('Roles', true, NULL, NULL, NOW(), NOW()),
+('Permissions', true, NULL, NULL, NOW(), NOW()),
+('Requests', true, NULL, NULL, NOW(), NOW()) ;
+
+
+
+
