5728d1a971faddee7da458ad8a3c923526ad8e0d
shashi kant <skntjee@gmail.com>
2025-03-25 03:22:44 +0530

    added  avatar and api

--- src/controller/customer/customer.controller.ts
+++ src/controller/customer/customer.controller.ts
@@ -5,6 +5,7 @@ import { failureResponse, succesResponse } from "../../config/utils"
 import ownerService from "../../services/owner/owner.service"
 import { KycStatus } from "@prisma/client"
 import customerService from "../../services/customer/customer.service"
+import config from "../../config/config"
 
 const customerController  = {
     
@@ -24,6 +25,25 @@ const customerController  = {
             return  failureResponse({data:err}, res )
            }
        } ,
+
+
+       updateCustomerDetails : async function (req:Request, res:Response):Promise<any> {
+         try {
+          
+              const {userId , username} = req.userObj
+              const {first_name ,  last_name , avatar=config.defaulProfileUrl} = req.body
+ 
+             let customerDetailsUpdate =await customerService.updateCustomerDetails({userId: Number(userId), params:{first_name:first_name??"random_first_name" , last_name: last_name??"random_last_name" , avatar  } })
+             if(!customerDetailsUpdate.status)
+                return succesResponse({data:customerDetailsUpdate.data, message:"" } , res )  
+           
+             return succesResponse({data:customerDetailsUpdate.data, message:"Inserted " } , res )  
+            
+            }catch(err) {
+             console.log(err)
+             return  failureResponse({data:err}, res )
+            }
+        } ,
 
 }
 
