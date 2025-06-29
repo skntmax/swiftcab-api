2c3dbdd2b82872fda18193d2e4d87b8e23054173
shashi kant <skntjee@gmail.com>
2025-06-29 21:22:41 +0530

    added api and many nore

--- prisma/queries/type_of_vhicles.sql
+++ prisma/queries/type_of_vhicles.sql
@@ -9,3 +9,12 @@ INSERT INTO public.type_of_vhicle (vhicle_type, disc, updated_on, avatar) VALUES
 ('Tractor', 'Heavy-duty vehicle used in agriculture and construction', NOW(), 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653254/ambulance_itwcxf.png'),
 ('Ambulance', 'Emergency vehicle used for medical transport', NOW(), 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653254/ambulance_itwcxf.png'),
 ('Fire Truck', 'Emergency vehicle equipped to handle fire emergencies', NOW(), 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653254/fire-truck_yqjvb2.png');
+
+INSERT INTO public.type_of_vhicle (vhicle_type, disc, updated_on, is_active, avatar)
+VALUES 
+  ('bike', 'Bike (Swiftcab)', CURRENT_TIMESTAMP, true, 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1751209236/png-clipart-erik-buell-racing-motorcycle-ebr-1190rs-fim-superbike-world-championship-sport-bike-racing-motorbike-s-exhaust-system-car-thumbnail_gtjwbx.png'),
+  ('cabEconomy', 'Cab Economy', CURRENT_TIMESTAMP, true, 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653256/11_gqvdrc.png'),
+  ('cabXL', 'Cab XL', CURRENT_TIMESTAMP, true, 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653255/2_toed5p.png'),
+  ('autoNCR', 'Auto (NCR)', CURRENT_TIMESTAMP, true, 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1751209237/auto_zdiutr.png'),
+  ('cabPremium', 'Cab Premium', CURRENT_TIMESTAMP, true, 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653257/33_bkyvma.png'),
+  ('sharedCab', 'Shared Cab', CURRENT_TIMESTAMP, true, 'https://res.cloudinary.com/dw0hahzkn/image/upload/v1745653255/0_ogjocj.png');
